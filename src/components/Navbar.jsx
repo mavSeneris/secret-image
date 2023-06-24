d0ebfccf76c2eaa25b82d5fae55801b63191bf1e
Maverick <mavseneris@gmail.com>
2023-06-25 07:22:49 +0800

    secret image is added to image page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,11 @@
 import { Link } from "react-router-dom";
 
-
 export default function Navbar() {
   return (
     <div className="nav">
-        <Link to="/">Home</Link>
-        <Link to="/image">Image</Link>
-        <Link to="/login">Login</Link>
-
-
+      <Link to="/">Home</Link>
+      <Link to="/image">Image</Link>
+      <Link to="/login">Login</Link>
     </div>
-  )
+  );
 }
