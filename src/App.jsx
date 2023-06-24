84a2debb3068cf42704ff45d2649638ff1df71ac
Maverick <mavseneris@gmail.com>
2023-06-25 07:07:20 +0800

    initial layout ready

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,16 @@ import {
 import "./App.css";
 import Layout from "./components/Layout";
 import Home from "./pages/Home";
+import Image from "./pages/Image";
+import Login from "./pages/Login";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<Layout />}>
       <Route index element={<Home />} />
+      <Route path="image" element={<Image />} />
+      <Route path="login" element={<Login />} />
+
     </Route>
   )
 );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,14 @@
+import { Link } from "react-router-dom";
 
 
 export default function Navbar() {
   return (
-    <div className="nav">Navbar</div>
+    <div className="nav">
+        <Link to="/">Home</Link>
+        <Link to="/image">Image</Link>
+        <Link to="/login">Login</Link>
+
+
+    </div>
   )
 }
